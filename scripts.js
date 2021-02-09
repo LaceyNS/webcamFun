82e9648d9ccb41f7e67cacfcff3304a1257cef3e
Lacey Savage <lacey.savage@gmail.com>
2021-02-09 13:29:15 -0500

    add greenScreen function

--- scripts.js
+++ scripts.js
@@ -26,11 +26,17 @@ function paintToCanvas() {
 
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+
         //Take the pixels out
         let pixels = ctx.getImageData(0, 0, width, height);
+
         // Play with them
         // pixels = redEffect(pixels);
-        pixels = rgbSplit(pixels);
+        // pixels = rgbSplit(pixels);
+        //Create a ghosting pattern 
+        // ctx.globalAlpha = 0.1;
+        pixels = greenScreen(pixels);
+
         //Put the pixels back
         ctx.putImageData(pixels, 0, 0);
     }, 16);
@@ -66,6 +72,36 @@ function rgbSplit(pixels) {
         pixels.data[i + 100] = pixels.data[i + 1]; //green
         pixels.data[i - 150] = pixels.data[i + 2]; //blue
     }
+    return pixels;
+}
+
+
+function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll(".rgb input").forEach((input) => {
+        levels[input.name] = input.value;
+    });
+
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if (
+            red >= levels.rmin 
+            && green >= levels.gmin 
+            && blue >= levels.bmin 
+            && red <= levels.rmax 
+            && green <= levels.gmax 
+            && blue <= levels.bmax) {
+
+            // take it out!
+            pixels.data[i + 3] = 0;
+        }
+    }
+
     return pixels;
 }
 
