029da6c89bae13639522b50be660f74220c2e8c3
Lacey Savage <lacey.savage@gmail.com>
2021-02-09 13:02:38 -0500

    add redEffect function

--- scripts.js
+++ scripts.js
@@ -25,7 +25,13 @@ function paintToCanvas() {
     canvas.height = height;
 
     return setInterval(() => {
-        ctx.drawImage(video, 0, 0, width, height)
+        ctx.drawImage(video, 0, 0, width, height);
+        //Take the pixels out
+        let pixels = ctx.getImageData(0, 0, width, height);
+        // Play with them
+        pixels = redEffect(pixels);
+        //Put the pixels back
+        ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
 
@@ -42,6 +48,15 @@ function takePhoto() {
     link.innerHTML = `<img src="${data}" alt="Tired Developer" />`;
     // This is how to do jQuery prepend in vanilla JS
     strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 100; //red
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; //green
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5;//blue
+    }
+    return pixels;
 }
 
 getVideo();
